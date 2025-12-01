dcd4e3754f7bf006ad7866b0cbb0e81dd48bfdb6
King-zwan <jimjeb22@gmail.com>
2025-12-01 13:53:50 +0200

    Delete src/main/java/com/project/cinematch/cinematch directory

--- src/main/java/com/project/cinematch/cinematch/Model/KpiResult.java
+++
@@ -1,9 +0,0 @@
-package com.project.cinematch.Model;
-
-public class KpiResult {
-    private Long id;
-    private double value;
-    private String type; // π.χ. "STAR_POWER", "BOX_OFFICE_PROXY"
-
-    // getters / setters / constructor
-}
