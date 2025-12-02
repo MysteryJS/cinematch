1379dfbb380fc8b102e9b39c49ab6e795d872a47
Lemmonadas <massaropanos@gmail.com>
2025-12-02 23:06:45 +0200

    forgor

--- src/test/test_sentimentanalysis.py
+++ src/test/test_sentimentanalysis.py
@@ -1,4 +1,10 @@
+import sys
+import os
 import pytest
+
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../sentiment_analysis")))
+
 from sentiment_analysis import sentiment_analysis
 
 app = sentiment_analysis.app
