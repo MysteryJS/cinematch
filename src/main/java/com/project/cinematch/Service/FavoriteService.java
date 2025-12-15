1b3fae08550ad2d472307bea4c45bf9a8cc009d2
Eresh69 <doggocookie@gmail.com>
2025-12-15 20:41:26 +0200

    garana

--- src/main/java/com/project/cinematch/Service/FavoriteService.java
+++ src/main/java/com/project/cinematch/Service/FavoriteService.java
@@ -2,7 +2,9 @@ package com.project.cinematch.Service;
 
 import com.project.cinematch.Model.Favorite;
 import com.project.cinematch.Model.FavoriteId;
+import com.project.cinematch.Model.User;
 import com.project.cinematch.Repository.FavoriteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,27 +12,41 @@ import java.util.List;
 @Service
 public class FavoriteService {
 
-    private final FavoriteRepository repo;
+    @Autowired
+    private FavoriteRepository favoriteRepository;
 
-    public FavoriteService(FavoriteRepository repo) {
-        this.repo = repo;
-    }
+    public Favorite addFavorite(User user, String movieId, String movieTitle) {
+        FavoriteId favoriteId = new FavoriteId(user.getId().intValue(), movieId);
 
-    public void addFavorite(Integer userId, String movieId) {
-        FavoriteId id = new FavoriteId(userId, movieId);
-        if (!repo.existsById(id)) {
-            repo.save(new Favorite(id));
+        if (favoriteRepository.existsById(favoriteId)) {
+            return null; // already favorited
         }
+
+        Favorite favorite = new Favorite();
+        favorite.setId(favoriteId);
+        favorite.setMovieTitle(movieTitle);
+
+        return favoriteRepository.save(favorite);
     }
 
-    public void removeFavorite(Integer userId, String movieId) {
-        repo.deleteById(new FavoriteId(userId, movieId));
+    public List<Favorite> getFavoritesByUser(User user) {
+        return favoriteRepository.findByIdUserId(user.getId().intValue());
     }
 
-    public List<String> getUserFavorites(Integer userId) {
-        return repo.findByIdUserId(userId)
-                .stream()
-                .map(f -> f.getId().getMovieId())
-                .toList();
+    public boolean removeFavorite(User user, String movieId) {
+        FavoriteId favoriteId = new FavoriteId(user.getId().intValue(), movieId);
+
+        if (!favoriteRepository.existsById(favoriteId)) {
+            return false;
+        }
+
+        favoriteRepository.deleteById(favoriteId);
+        return true;
+    }
+
+    public boolean isFavorite(User user, String movieId) {
+        FavoriteId favoriteId = new FavoriteId(user.getId().intValue(), movieId);
+        return favoriteRepository.existsById(favoriteId);
     }
 }
+
