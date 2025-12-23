98dbd31f0df6a795f6518bf15ea0e0cba75fda2e
MysteryJS <dkaza2005@gmail.com>
2025-12-24 01:39:01 +0200

    changed forum ui + added correct verification

--- src/main/java/com/project/cinematch/Controller/SecurityConfig.java
+++ src/main/java/com/project/cinematch/Controller/SecurityConfig.java
@@ -7,13 +7,14 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/", "/login", "/logo.png", "/css/**","/search").permitAll()
-                .requestMatchers("/quiz").authenticated()
+                .requestMatchers("/", "/login", "/logo.png", "/css/**","/search", "/trending", "/face", "/sentiment", "/TOU").permitAll()
+                .requestMatchers("/quiz", "/forum").authenticated()
                 .anyRequest().permitAll()
             )
             .formLogin(form -> form
@@ -22,4 +23,5 @@ public class SecurityConfig {
             );
         return http.build();
     }
+    
 }
