020f9235addac5905eb11bc37db8311e157cfb57
King-zwan <jimjeb22@gmail.com>
2025-12-23 19:02:38 +0200

    Add files via upload

---
+++ src/main/java/com/project/cinematch/Repository/ForumMediaRepository.java
@@ -0,0 +1,7 @@
+package com.project.cinematch.Repository;
+
+import com.project.cinematch.Model.ForumMedia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ForumMediaRepository extends JpaRepository<ForumMedia, Long> {
+}
